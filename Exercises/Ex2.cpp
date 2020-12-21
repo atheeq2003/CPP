9c3ea208dddb48577a30878720a03e7a6ba08679
atheeq2003 <atheequrrahaman8@gmail.com>
2020-12-21 19:08:38 +0530

    Exercise2

---
+++ Exercises/Ex2.cpp
@@ -0,0 +1,21 @@
+/* Program to get input from user: 
+That is: Name, age, mark1, mark2, mark3
+And Output the total of three Marks */
+#include <iostream>
+int main()
+{
+    std::string name;
+    int mark1, mark2, mark3, age, total;
+    std::cout << "Enter the Name: \n";
+    std::cin >> name;
+    std::cout << "Enter the Age: \n";
+    std::cin >> age;
+    std::cout << "Enter Mark1: \n";
+    std::cin >> mark1;
+    std::cout << "Enter Mark2: \n";
+    std::cin >> mark2;
+    std::cout << "Enter Mark3: \n";
+    std::cin >> mark3;
+    total = mark1 + mark2 + mark3;
+    std::cout << "Total= \n" << total;
+}
