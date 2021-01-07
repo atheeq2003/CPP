6b1ecfcc450b465cd3f73b5f02fe33e008624dbd
atheeq2003 <atheequrrahaman8@gmail.com>
2021-01-07 17:59:36 +0530

    Exercise-5

---
+++ Exercises/Ex5.cpp
@@ -0,0 +1,12 @@
+// While Loop
+#include <iostream>
+using namespace std; // Use this to use cout and cin with using std
+int main()
+{
+    int a = 10;
+    while (a >= 1)
+    {
+        cout << a << "\n";
+        a--;
+    }
+}
