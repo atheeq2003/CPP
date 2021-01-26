91bb4cd4b7d5ae563d745cc24e26cb829c542939
atheeq2003 <atheequrrahaman8@gmail.com>
2021-01-26 18:25:36 +0530

    BasicCalculator

---
+++ ExternCode_C++/4.BasicCalculator.cpp
@@ -0,0 +1,14 @@
+// Basic Calculator; cin;cout;
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout << "Enter The First Number: \n";
+    cin >> a;
+    cout << "Enter The Second Number: \n";
+    cin >> b;
+    sum = a + b;
+    cout << "The Sum Is: " << sum;
+    return 0;
+}
