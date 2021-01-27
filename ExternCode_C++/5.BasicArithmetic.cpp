83d987ee02798a0df00b5fbf47116875cc77150f
atheeq2003 <atheequrrahaman8@gmail.com>
2021-01-27 13:42:56 +0530

    BasicArithmetic

---
+++ ExternCode_C++/5.BasicArithmetic.cpp
@@ -0,0 +1,11 @@
+// Basic Arithmetic
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 5 + 7 + 8; 
+    int b = (5 + 7) * 8; 
+    cout << a << endl;
+    cout << b;
+    return 0;
+}
