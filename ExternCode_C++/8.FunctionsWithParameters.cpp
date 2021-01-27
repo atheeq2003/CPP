29c210f91242f21e7c24c378790d18d1d4df1da5
atheeq2003 <atheequrrahaman8@gmail.com>
2021-01-27 18:32:55 +0530

    FunctionsWithParameters

---
+++ ExternCode_C++/8.FunctionsWithParameters.cpp
@@ -0,0 +1,12 @@
+// Functions with parameters
+#include<iostream>
+using namespace std;
+void example(int a){
+    cout << "I have " << a << " chocolates" << endl;
+}
+int main()
+{
+    example(10);
+    example(5);
+    return 0;
+}
