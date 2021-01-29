7d369ad760164246907a0a5902b4d34d2fd636fc
atheeq2003 <atheequrrahaman8@gmail.com>
2021-01-29 17:46:23 +0530

    VariablesInClasses

--- ExternCode_C++/10.ClassesAndObjects.cpp
+++ ExternCode_C++/10.ClassesAndObjects.cpp
@@ -1,9 +1,9 @@
 // Objects and Classes
 #include<iostream>
 using namespace std;
-class AtheeqsClass{              // Create/Declare a class with name of your choice
-    public:                      // Use public to use anywhere in the whole program
-        void greetings(){
+class AtheeqsClass{          // Create/Declare a class with name of your choice
+    public:                  // This is called access modifier. There are two types:Public and Private
+        void greetings(){    // Use public to use the function anywhere in the whole program
             cout << "Its nice to meet you!";
         }
 };
@@ -11,4 +11,4 @@ int main(){
     AtheeqsClass AtheeqsObject;  // Give name to your object
     AtheeqsObject.greetings();   // use dot seperator to use the function
     return 0;
-}
+} 
---
+++ ExternCode_C++/11.VariablesInClasses.cpp
@@ -0,0 +1,19 @@
+// Variables in Classes
+#include<iostream>
+using namespace std;
+class AtheeqsClass{
+    public:
+        void setname(string x){
+            name = x;
+        }
+             string getname(){
+                 return name;
+             }
+    private:
+        string name;
+};
+int main(){
+    AtheeqsClass AtheeqsObject;
+    AtheeqsObject.setname("Atheequr Rahaman");
+    cout << AtheeqsObject.getname();
+}
