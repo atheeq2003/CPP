c2bcc6970241f4e5c74b844ad6fe8c1e4315fdd6
atheeq2003 <atheequrrahaman8@gmail.com>
2021-02-19 17:04:49 +0530

    ArraysWithLoops

---
+++ ExternCode_C++/29.ArraysWithLoops.cpp
@@ -0,0 +1,11 @@
+// Arrays With loops
+#include<iostream>
+using namespace std;
+int main(){
+    int atheeq[11];
+    int x;
+    for(x = 0; x <= 10;x++){
+        atheeq[x] = 15;
+        cout << x << " ----- " << atheeq[x] << endl;
+    }
+}
