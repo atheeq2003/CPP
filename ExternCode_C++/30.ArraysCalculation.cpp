a7f0482c0e341a3d4e96401d7897d91fb5de4f50
atheeq2003 <atheequrrahaman8@gmail.com>
2021-02-19 18:51:24 +0530

    ArraysCalculation

---
+++ ExternCode_C++/30.ArraysCalculation.cpp
@@ -0,0 +1,13 @@
+// Arrays Calculation
+#include<iostream>
+using namespace std;
+int main(){
+    int atheeq[5] = {5, 10, 15, 20, 25};
+    int sum = 0;
+    for(int x = 0; x < 5; x++){
+        sum = sum + atheeq[x];
+    }
+    cout << "Sum = " << sum << endl;
+    
+    
+}
