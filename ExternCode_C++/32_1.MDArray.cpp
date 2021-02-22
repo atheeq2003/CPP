b77483de93968e540eddc8c2b8de983d210e7481
atheeq2003 <atheequrrahaman8@gmail.com>
2021-02-22 16:48:38 +0530

    MDArray-1

---
+++ ExternCode_C++/32_1.MDArray.cpp
@@ -0,0 +1,9 @@
+// MD Array-Multi Dimensional Array
+#include<iostream>
+using namespace std;
+int main(){
+    int atheeq[2][4] = {{5,8,6,7},{10,9,6,2}};
+    // 5 8 6 7
+    // 10 9 6 2
+    cout << atheeq[1][3] << endl;
+}
