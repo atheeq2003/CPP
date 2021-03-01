81726988f8ec2397dd926ef0e09eea347dea2a96
atheeq2003 <atheequrrahaman8@gmail.com>
2021-03-01 17:26:15 +0530

    Sizeof

---
+++ ExternCode_C++/35.Sizeof.cpp
@@ -0,0 +1,8 @@
+// Size of
+#include<iostream>
+using namespace std;
+int main(){
+    double atheeq[5];
+    cout << sizeof(atheeq) << endl;
+    cout << sizeof(atheeq) / sizeof(atheeq[0]) << endl;
+}
